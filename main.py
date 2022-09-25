4f1b42219703b28f31aae9607aea1990477146c8
Tomasz Madej <tomasz@madej.ovh>
2022-09-25 12:38:03 +0200

    Add print shop and stuff

--- main.py
+++ main.py
@@ -2,3 +2,15 @@ lista = {
     'piekarni': ['chleb', 'bułki', 'pączka'],
     'warzywniaka': ['marchew', 'seler', 'rukolę']
 }
+things = 0
+
+for shop, stuff in list(lista.items()):
+    stuff_up = []
+
+    for i in range(len(stuff)):
+        x = stuff[i]
+        y = x.capitalize()
+        stuff_up.append(y)
+        things = things + 1
+
+    print('Idę do', shop.capitalize(), f'i kupuję tu następujące rzeczy: {stuff_up}.')
